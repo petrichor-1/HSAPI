28d4d0d3d9a56cbd38d8a6067fb4c254360cb86d
Petrichor <60892756+petrichor-1@users.noreply.github.com>
2025-02-18 18:55:26 -0600

    Split tests

--- Tests/HSAPITests/HSAPITests.swift
+++ Tests/HSAPITests/HSAPITests.swift
@@ -2,37 +2,30 @@ import XCTest
 @testable import HSAPI
 
 final class HSAPITests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-        if true {
-            guard let url = Bundle.module.url(forResource: "connectthepipes", withExtension: "hopscotch") else {
-                XCTFail("No connect the pipes project json url")
-                return
-            }
-            guard let ctpData = try? Data(contentsOf: url) else {
-                XCTFail("No connect the pipes project json")
-                return
-            }
-            let decoder = JSONDecoder()
-            let project = try decoder.decode(LowLevelProject.self, from: ctpData)
-            XCTAssertNotNil(project)
+    func testConnectThePipesLowLevelDecoding() throws {
+        guard let url = Bundle.module.url(forResource: "connectthepipes", withExtension: "hopscotch") else {
+            XCTFail("No connect the pipes project json url")
+            return
         }
-        if true {
-            guard let url = Bundle.module.url(forResource: "mooseisland", withExtension: "hopscotch") else {
-                XCTFail("No mooseisland project json url")
-                return
-            }
-            guard let ctpData = try? Data(contentsOf: url) else {
-                XCTFail("No mooseisland project json")
-                return
-            }
-            let decoder = JSONDecoder()
-            let project = try decoder.decode(LowLevelProject.self, from: ctpData)
-            XCTAssertNotNil(project)
+        guard let ctpData = try? Data(contentsOf: url) else {
+            XCTFail("No connect the pipes project json")
+            return
         }
+        let decoder = JSONDecoder()
+        let project = try decoder.decode(LowLevelProject.self, from: ctpData)
+        XCTAssertNotNil(project)
+    }
+    func testMooseIslandLowLevelDecoding() throws {
+        guard let url = Bundle.module.url(forResource: "mooseisland", withExtension: "hopscotch") else {
+            XCTFail("No mooseisland project json url")
+            return
+        }
+        guard let ctpData = try? Data(contentsOf: url) else {
+            XCTFail("No mooseisland project json")
+            return
+        }
+        let decoder = JSONDecoder()
+        let project = try decoder.decode(LowLevelProject.self, from: ctpData)
+        XCTAssertNotNil(project)
     }
 }
